bf06e3e280ecb80141b19542c1c68235ae5046b4
mi1128h <70762557+mi1128h@users.noreply.github.com>
2020-10-20 23:02:21 +0900

    로고, 타이틀 스테이트 추가

---
+++ 기말프로젝트/logo_state.py
@@ -0,0 +1,46 @@
+import gfw
+from pico2d import *
+import title_state
+import gobj
+
+canvas_width = 1280
+canvas_height = 720
+elapsed = 0
+
+def enter():
+    gfw.world.init(['logo'])
+    logo = gobj.ImageObject('logo.png', (canvas_width // 2, canvas_height // 2))
+    gfw.world.add(gfw.layer.logo, logo)
+
+def update():
+    gfw.world.update()
+    global elapsed
+    elapsed += gfw.delta_time
+    if elapsed > 2.0:
+        gfw.change(title_state)
+
+def draw():
+    gfw.world.draw()
+
+def handle_event(e):
+    if e.type == SDL_QUIT:
+        gfw.quit()
+    elif e.type == SDL_KEYDOWN:
+        if e.key == SDLK_ESCAPE:
+            gfw.quit()
+    pass
+
+def exit():
+    pass
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+if __name__ == '__main__':
+    gfw.run_main()
---
+++ 기말프로젝트/title_state.py
@@ -0,0 +1,59 @@
+import gfw
+from pico2d import *
+import main_state
+import gobj
+
+canvas_width = 1280
+canvas_height = 720
+select = 'start'
+
+def enter():
+    global menu_pointer
+    gfw.world.init(['title_bg', 'menu_pointer'])
+    title_bg = gobj.ImageObject('title_1280.png', (canvas_width // 2, canvas_height // 2))
+    gfw.world.add(gfw.layer.title_bg, title_bg)
+
+    menu_pointer = gobj.ImageObject('menu_pointer.png', (canvas_width // 2, canvas_height // 4))
+    gfw.world.add(gfw.layer.menu_pointer, menu_pointer)
+
+def update():
+    gfw.world.update()
+
+def draw():
+    gfw.world.draw()
+
+
+def handle_event(e):
+    global select, menu_pointer
+    if e.type == SDL_QUIT:
+        gfw.quit()
+    elif e.type == SDL_KEYDOWN:
+        if e.key == SDLK_ESCAPE:
+            gfw.quit()
+        elif e.key == SDLK_UP:
+            select = 'start'
+            menu_pointer.pos = (canvas_width // 2, canvas_height // 4)
+        elif e.key == SDLK_DOWN:
+            select = 'exit'
+            menu_pointer.pos = (canvas_width // 2, canvas_height // 6 + 17)
+        elif e.key == SDLK_RETURN:
+            if select == 'start':
+                gfw.push(main_state)
+            elif select == 'exit':
+                gfw.quit()
+
+
+def exit():
+    pass
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+if __name__ == '__main__':
+    gfw.run_main()
