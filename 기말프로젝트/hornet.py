f1e60a56cc6e24109db60bb1c5341afed95210d2
Mihyeon Kim <mihyeon001128@gmail.com>
2020-11-25 01:15:01 +0900

    hornet 패턴 추가

--- 기말프로젝트/hornet.py
+++ 기말프로젝트/hornet.py
@@ -10,10 +10,12 @@ class Hornet:
     images = {}
     sounds = {}
     ACTIONS = ['Dash', 'Dash end', 'Dash ready', 'Idle', 'Jump', 'Jump ready', 'Land',
-               'Run', 'Sphere', 'Sphere ball', 'Sphere end', 'Sphere ready']
-    DASH_COOL = 2
-    JUMP_COOL = 5
-    SPHERE_COOL = 7
+               'Run', 'Sphere', 'Sphere end', 'Sphere ready']
+    RUN_MAX_TIME = 1
+    DASH_MAX_TIME = 0.5
+    DASH_COOL = 1
+    JUMP_COOL = 3
+    SPHERE_COOL = 6
     FPS = 10
 
     def __init__(self):
@@ -23,23 +25,27 @@ class Hornet:
         self.fall_image = None
         self.action = 'Idle'
         self.time = 0
+        self.run_time = 0
         self.dash_time = 0
-        self.jump_time = 0
-        self.sphere_time = 0
+        self.dash_cool_time = 0
+        self.jump_cool_time = 0
+        self.sphere_cool_time = 0
         self.fidx = 0
+        self.flip = 'h'
         self.target = None
         self.health = 300
         self.build_behavior_tree()
 
     def draw(self):
         pos = self.bg.to_screen(self.pos)
-        flip = '' if self.delta[0] < 0 else 'h'
+        if self.action != 'Dash ready' and self.action != 'Dash' and self.action != 'Dash end':
+            self.flip = '' if self.delta[0] < 0 else 'h'
         if self.action != 'Fall':
             images = self.images[self.action]
             image = images[self.fidx]
         else:
             image = self.fall_image
-        image.composite_draw(0, flip, *pos, image.w, image.h)
+        image.composite_draw(0, self.flip, *pos, image.w, image.h)
 
     def update(self):
         x, y = self.pos
@@ -47,6 +53,9 @@ class Hornet:
         x = clamp(bg_l, x, bg_r)
         y = clamp(bg_b, y, bg_t)
         self.pos = x, y
+        self.jump_cool_time += gfw.delta_time
+        self.dash_cool_time += gfw.delta_time
+        self.sphere_cool_time += gfw.delta_time
         self.bt.run()
 
     def idle(self):
@@ -55,10 +64,8 @@ class Hornet:
         self.time += gfw.delta_time
         frame = self.time * Hornet.FPS
         self.fidx = int(frame) % len(self.images[self.action])
-        if gobj.distance(self.pos, self.target.pos) < 200:
-            self.action = 'Jump ready'
-            self.time = 0
-            self.fidx = 0
+        if gobj.distance(self.pos, self.target.pos) < 800:
+            self.action = 'Run'
         return BehaviorTree.SUCCESS
 
     def wounded(self):
@@ -78,9 +85,9 @@ class Hornet:
         if self.action == 'Jump' or self.action == 'Fall' or self.action == 'Land':
             return BehaviorTree.SUCCESS
         if self.action != 'Jump ready':
-            if self.jump_time > Hornet.JUMP_COOL:
+            if self.jump_cool_time > Hornet.JUMP_COOL:
                 self.action = 'Jump ready'
-                self.jump_time = 0
+                self.jump_cool_time = 0
                 self.time = 0
             else:
                 return BehaviorTree.FAIL
@@ -96,7 +103,7 @@ class Hornet:
             else:
                 self.delta = (-3, 30)
             return BehaviorTree.SUCCESS
-        return BehaviorTree.FAIL
+        return BehaviorTree.RUNNING
 
     def jump(self):
         if self.action == 'Fall' or self.action == 'Land':
@@ -118,7 +125,7 @@ class Hornet:
             images = self.images['Jump']
             self.fall_image = images[len(images) - 1]
             return BehaviorTree.SUCCESS
-        return BehaviorTree.FAIL
+        return BehaviorTree.RUNNING
 
     def fall(self):
         if self.action == 'Land':
@@ -137,7 +144,7 @@ class Hornet:
             self.pos = x, 480
             self.action = 'Land'
             return BehaviorTree.SUCCESS
-        return BehaviorTree.FAIL
+        return BehaviorTree.RUNNING
 
     def land(self):
         if self.action != 'Land':
@@ -150,7 +157,139 @@ class Hornet:
         if self.fidx >= len(self.images['Land']) - 1:
             self.action = 'Idle'
             return BehaviorTree.SUCCESS
-        return BehaviorTree.FAIL
+        return BehaviorTree.RUNNING
+
+    def sphere_ready(self):
+        if self.action == 'Sphere' or self.action == 'Sphere end':
+            return BehaviorTree.SUCCESS
+        if self.action != 'Sphere ready':
+            if self.sphere_cool_time > Hornet.SPHERE_COOL:
+                self.action = 'Sphere ready'
+                self.sphere_cool_time = 0
+                self.time = 0
+            else:
+                return BehaviorTree.FAIL
+
+        self.time += gfw.delta_time
+        frame = self.time * Hornet.FPS
+        self.fidx = int(frame) % len(self.images[self.action])
+        if self.fidx == len(self.images['Sphere ready']) - 1:
+            self.action = 'Sphere'
+            self.time = 0
+            return BehaviorTree.SUCCESS
+        return BehaviorTree.RUNNING
+
+    def sphere(self):
+        if self.action == 'Sphere end':
+            return BehaviorTree.SUCCESS
+        if self.action != 'Sphere':
+            return BehaviorTree.FAIL
+
+        self.time += gfw.delta_time
+        frame = self.time * Hornet.FPS
+        self.fidx = int(frame) % len(self.images[self.action])
+        if self.fidx == len(self.images['Sphere']) - 1:
+            self.action = 'Sphere end'
+            self.time = 0
+            return BehaviorTree.SUCCESS
+        return BehaviorTree.RUNNING
+
+    def sphere_end(self):
+        if self.action != 'Sphere end':
+            return BehaviorTree.FAIL
+
+        self.time += gfw.delta_time
+        frame = self.time * Hornet.FPS
+        self.fidx = int(frame) % len(self.images[self.action])
+        if self.fidx == len(self.images['Sphere end']) - 1:
+            self.action = 'Fall'
+            self.time = 0
+            return BehaviorTree.SUCCESS
+        return BehaviorTree.RUNNING
+
+    def run(self):
+        if self.action != 'Run':
+            self.run_time = random.random()
+            self.action = 'Run'
+        if self.pos[0] < self.target.pos[0]:
+            self.delta = -3, 0
+        else:
+            self.delta = 3, 0
+
+        gobj.move_obj(self)
+        self.time += gfw.delta_time
+        self.run_time += gfw.delta_time
+        frame = self.time * Hornet.FPS
+        self.fidx = int(frame) % len(self.images[self.action])
+
+        if self.run_time > Hornet.RUN_MAX_TIME:
+            self.delta = 0, 0
+            return BehaviorTree.SUCCESS
+        return BehaviorTree.RUNNING
+
+    def dash_ready(self):
+        if self.action == 'Dash' or self.action == 'Dash end':
+            return BehaviorTree.SUCCESS
+        if self.action != 'Dash ready':
+            if self.dash_cool_time > Hornet.DASH_COOL:
+                self.action = 'Dash ready'
+                self.dash_cool_time = 0
+                self.time = 0
+                if self.pos[0] < self.target.pos[0]:
+                    self.flip = 'h'
+                else:
+                    self.flip = ''
+            else:
+                return BehaviorTree.FAIL
+
+        self.time += gfw.delta_time
+        frame = self.time * Hornet.FPS
+        self.fidx = int(frame) % len(self.images[self.action])
+        if self.fidx == len(self.images['Dash ready']) - 1:
+            self.action = 'Dash'
+            self.time = 0
+            if self.pos[0] < self.target.pos[0]:
+                self.delta = 20, 0
+            else:
+                self.delta = -20, 0
+            return BehaviorTree.SUCCESS
+        return BehaviorTree.RUNNING
+
+    def dash(self):
+        if self.action == 'Dash end':
+            return BehaviorTree.SUCCESS
+        if self.action != 'Dash':
+            return BehaviorTree.FAIL
+
+        gobj.move_obj(self)
+        self.dash_time += gfw.delta_time
+        self.time += gfw.delta_time
+        frame = self.time * Hornet.FPS
+        self.fidx = int(frame) % len(self.images[self.action])
+
+        if self.dash_time > Hornet.DASH_MAX_TIME:
+            self.dash_time = 0
+            self.action = 'Dash end'
+            self.delta = 0, 0
+            self.time = 0
+            return BehaviorTree.SUCCESS
+        return BehaviorTree.RUNNING
+
+    def dash_end(self):
+        if self.action != 'Dash end':
+            return BehaviorTree.FAIL
+
+        self.time += gfw.delta_time
+        frame = self.time * Hornet.FPS
+        self.fidx = int(frame) % len(self.images[self.action])
+        if self.fidx == len(self.images['Dash end']) - 1:
+            self.action = 'Idle'
+            self.time = 0
+            return BehaviorTree.SUCCESS
+        return BehaviorTree.RUNNING
+
+    def do_nothing(self):
+        return BehaviorTree.SUCCESS
 
     def build_behavior_tree(self):
         self.bt = BehaviorTree.build({
@@ -182,6 +321,39 @@ class Hornet:
                             "function": self.jump,
                         },
                         {
+                            "name": "sphere or fall",
+                            "class": SelectorNode,
+                            "children": [
+                                {
+                                    "name": "Sphere Attack",
+                                    "class": SequenceNode,
+                                    "children": [
+                                        {
+                                            "name": "sphere ready",
+                                            "class": LeafNode,
+                                            "function": self.sphere_ready,
+                                        },
+                                        {
+                                            "name": "sphere",
+                                            "class": LeafNode,
+                                            "function": self.sphere,
+                                        },
+                                        {
+                                            "name": "sphere end",
+                                            "class": LeafNode,
+                                            "function": self.sphere_end,
+                                        },
+                                    ],
+                                },
+                                {
+                                    "name": "Do nothing",
+                                    "class": LeafNode,
+                                    "function": self.do_nothing,
+                                }
+                            ]
+                        },
+
+                        {
                             "name": "fall",
                             "class": LeafNode,
                             "function": self.fall,
@@ -192,6 +364,32 @@ class Hornet:
                             "function": self.land,
                         },
                     ],
+                },
+                {
+                    "name": "Dash",
+                    "class": SequenceNode,
+                    "children": [
+                        {
+                            "name": "dash ready",
+                            "class": LeafNode,
+                            "function": self.dash_ready,
+                        },
+                        {
+                            "name": "dash",
+                            "class": LeafNode,
+                            "function": self.dash,
+                        },
+                        {
+                            "name": "dash end",
+                            "class": LeafNode,
+                            "function": self.dash_end,
+                        },
+                    ],
+                },
+                {
+                    "name": "Run",
+                    "class": LeafNode,
+                    "function": self.run,
                 },
             ]
         })
@@ -219,3 +417,7 @@ class Hornet:
         Hornet.images = images
         print('Hornet %d images loaded' % (count))
         return images
+
+    def get_bb(self):
+        x, y = self.bg.to_screen(self.pos)
+        return x - 30, y - 50, x + 30, y + 50
